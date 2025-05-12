60eba95bea574fbe608ac49f9295c290b0ed6d2b
manishInstaMD <manishknatani@gmail.com>
2025-05-12 18:50:05 +0530

    12Second

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ app.options('*', cors(corsOptions));
 
 // Other middleware
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 // Static files
 app.use("/processed", express.static(path.join(__dirname, "processed")));
