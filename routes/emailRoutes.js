a838ded6c07e7d3ce7fcfde4d09fa0c957542aca
manishInstaMD <manishknatani@gmail.com>
2025-07-01 12:15:11 +0530

    list api add

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router(); // You missed this line
 const sendInviteEmail = require("../utils/mail2");
+const db = require("../config/database");
 
 router.post("/send-invite", async (req, res) => {
   const { toEmail, doctorName } = req.body;
@@ -15,6 +16,33 @@ router.post("/send-invite", async (req, res) => {
   } catch (error) {
     console.error("Error sending email:", error);
     res.status(500).json({ message: "Failed to send email", error: error.message });
+  }
+});
+
+
+// GET /api/users
+router.get('/alkenUser', async (req, res) => {
+  try {
+    const users = await db.models.Alkem.findAll();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' , error: err.message});
+  }
+});
+
+// DELETE /api/users/:id
+router.delete('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleted = await db.models.alkem.destroy({ where: { id } });
+
+    if (deleted) {
+      res.json({ message: 'User deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
